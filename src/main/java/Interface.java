3c23cd9b395677189a00e1f0e3df97bfb8dfe3a6
VCHEPKOIT <vchepkoit@safaricom.co.ke>
2019-05-31 15:43:54 +0300

    engineer constructors working

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -5,6 +5,27 @@ import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class Interface {
+    private String firstname;
+    private String lastname;
+    private int ek;
+    private String email;
+
+    public Interface(String firstname, String lastname, int ek, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.ek = ek;
+        this.email = email;
+    }
+
+    public String getFirstname() { return firstname; }
+    public String getLastName() { return lastname; }
+    public int getEk() { return ek; }
+    public String getEmail() {
+        return email;
+    }
+
+
+
 
     public static void main(String[] args) {
 
@@ -25,6 +46,8 @@ public class Interface {
         get("/faqs", (request, response) -> {
             return new ModelAndView(new HashMap(), "faqs.hbs");
         }, new HandlebarsTemplateEngine());
+
+
 
     }
 }
